09b1f886e9d7fb313b68868a45bbc495b5a625d0
Stacey Vetzal <stacey@mojility.ca>
2025-11-25 20:56:26 -0500

    fix: Add crypto polyfill for Jest tests in Node.js environment

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   testEnvironment: 'node',
   roots: ['<rootDir>/src'],
   testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  setupFiles: ['<rootDir>/jest.setup.js'],
   globals: {
     'ts-jest': {
       tsconfig: 'tsconfig.test.json',
